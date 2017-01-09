c80bfe416be491955bc5233d4fe2958f794949d0
GwenRaq <gwen.raq@gmail.com>
2017-01-09 08:42:55 -1000

    1/9

--- java/app.js
+++ java/app.js
@@ -1,4 +1,4 @@
-var fortuneList = ["You are unlucky.","You will pass your test", "I dont know"];
+var fortuneList = ["You are unlucky.","You will pass your test", "I dont know", "Never say GoodBye", " Your Dreams Will Come True"];
 
 function generateFortuneCookie(){
 	var node = document.createElement("li");
